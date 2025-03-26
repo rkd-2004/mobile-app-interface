1322a9a76d1faea57e335bd9e952cffea33eed30
rkd2004 <rkdsince2004@gmail.com>
2025-03-26 15:32:59 +0530

    Bug

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: 'dist'
   }
 })
